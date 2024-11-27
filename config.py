6c63c92e3bd8de91a661e3af0061a95d0765c73d
Байэл <bajeltairbekov3@gmail.com>
2024-11-27 14:13:11 +0600

    dz2

---
+++ config.py
@@ -0,0 +1,8 @@
+from decouple import config
+from aiogram import Bot, Dispatcher
+
+
+
+token = config('TOKEN')
+bot = Bot(token=token)
+dp = Dispatcher(bot)
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from decouple import config
-from aiogram import Dispatcher, Bot, executor, types
-import logging
-token = config('TOKEN')
-bot = Bot(token=token)
-dp = Dispatcher(bot)
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text=f'Hello {message.from_user.first_name}\n'
-                                f'Твой Telegram Id - {message.from_user.id}')
-    await message.answer('Привет')
+from aiogram import executor,types
+from config import dp
+from handlers import commands,quiz,game
+
+quiz.register_handler_quiz(dp)
+commands.register_commands(dp)
+game.register_game(dp)
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True)
